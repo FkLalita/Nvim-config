8f6e9476ee74608c7cc1280fce427396d3cdf03a
fklalita <FkLalita>
2024-03-20 02:07:45 +0100

    go

---
+++ init.lua
@@ -0,0 +1,24 @@
+require("fklalita.plugins")
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- Shorthand notation for GitHub; translates to https://github.com/junegunn/vim-easy-align
+Plug('junegunn/vim-easy-align')
+
+-- Any valid git URL is allowed
+Plug('https://github.com/junegunn/seoul256.vim.git')
+
+-- Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
+Plug('fatih/vim-go', { ['tag'] = '*' })
+
+-- Using a non-default branch
+Plug('neoclide/coc.nvim', { ['branch'] = 'release' })
+
+Plug 'wakatime/vim-wakatime'
+Plug 'theprimeagen/harpoon'
+
+vim.call('plug#end')
+
